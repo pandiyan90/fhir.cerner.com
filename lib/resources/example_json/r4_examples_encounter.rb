5b3ef084b2c28c6cedd625e1cb4dab49d0c6cf8e
Lauren Dumler <laurendumler1996@gmail.com>
2020-08-06 16:56:18 -0500

    encounter example updated with custom-attribute extension

--- lib/resources/example_json/r4_examples_encounter.rb
+++ lib/resources/example_json/r4_examples_encounter.rb
@@ -14,6 +14,20 @@ module Cerner
       },
       "extension": [
         {
+          "url": "https://fhir-ehr.cerner.com/r4/StructureDefinition/custom-attribute",
+          "extension": [
+              {
+                "url": "custom-attribute-name",
+                "id": "ENCNTR:3339152",
+                "valueString": "Self-Pay Follow Up Date"
+              },
+              {
+                "url": "custom-attribute-value",
+                "valueDateTime": "2020-07-03T12:00:00.000Z"
+              }
+            ]
+        },
+        {
           "url": "https://fhir-ehr.cerner.com/r4/StructureDefinition/client-organization",
           "valueReference": {
             "reference": "Organization/675844"
